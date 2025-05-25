55a2681f396b68af68b59e153a05e4790e8401a8
Raj Jaiswal <rajj91783@gmail.com>
2025-05-25 08:20:25 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-white bg-blue-700'>
+        Scribble Maniaaaaa
+      </h1>
+    </div>
+  )
+}
+
+export default App
