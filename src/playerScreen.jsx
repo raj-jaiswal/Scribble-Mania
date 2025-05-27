16218a4a2cbbbf4338a8c359de7b381f3c9615bf
Raj Jaiswal <rajj91783@gmail.com>
2025-05-27 21:41:42 +0530

    added playerMenu

---
+++ src/playerScreen.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import copyIcon from "./assets/copy-icon.svg"
+
+const PlayerScreen = () => {
+    return (
+        <div className="bg-white h-screen min-w-100 flex flex-col justify-center items-center">
+            <div className="text-3xl font-bold">Your Turn!</div>
+            <div className="text-lg font-semibold">Current word to Draw: { 'Echo' }</div>
+            <div className="font-semibold text-[#247FAC] cursor-pointer flex">
+                <a target="_blank" href={ import.meta.env.VITE_FIGMA_URL }>Draw in Figma</a>
+                <img src={copyIcon} className="h-6 w-6 ml-2 transition transform hover:-translate-y-0.5 hover:scale-105" onClick={ ()=>{navigator.clipboard.writeText(import.meta.env.VITE_FIGMA_URL);} }></img>
+            </div>
+        </div>
+    )
+}
+
+export default PlayerScreen;
