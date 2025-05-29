99ed69d5d01f1bf44b6c876b45536b5c04eeefb4
Raj Jaiswal <rajj91783@gmail.com>
2025-05-29 07:54:11 +0530

    fixed share screen

--- src/shareScreen.jsx
+++ src/shareScreen.jsx
@@ -6,7 +6,7 @@ const ShareScreen = ({ onClick, sharing }) => (
     className="m-3 px-6 py-3 rounded-lg bg-white text-black font-bold
                transition hover:-translate-y-1 hover:scale-105"
   >
-    {sharing ? 'Sharing…' : 'Share Screen'}
+    {sharing ? 'Stop Sharing' : 'Share Screen'}
   </button>
 );
 
